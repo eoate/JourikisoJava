32a539af04c51ac0a9b5b7c370228ef1384d30a8
eoate <tsubasa20060117@icloud.com>
2024-07-17 20:19:14 +0900

    a

---
+++ jourikisoJavaIntelliJ/src/jourikisoJava3/Shosyugou.java
@@ -0,0 +1,8 @@
+package jourikisoJava3;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class Shosyugou {
+    public static HashSet<HashSet<Integer>> quotientSet(HashSet<Integer> numbersA, HashSet<List<Integer>> numbersR, int n) {}
+}
