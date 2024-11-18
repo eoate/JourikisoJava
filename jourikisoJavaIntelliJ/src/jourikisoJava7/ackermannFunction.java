322032b923eb9385f48dbeae9498f9635458aeb5
eoate <tsubasa20060117@icloud.com>
2024-11-18 17:57:04 +0900

    ackermann

---
+++ jourikisoJavaIntelliJ/src/jourikisoJava7/ackermannFunction.java
@@ -0,0 +1,12 @@
+package jourikisoJava7;
+
+public class ackermannFunction {
+    public static int ackermann(int m,int n){
+        if(m==0)return n+1;
+        if(n==0)return ackermann(m-1,1);
+        return ackermann(m-1, ackermann(m,n-1));
+    }
+    public static void main(String[] args){
+        System.out.println(ackermann(3,1));
+    }
+}
